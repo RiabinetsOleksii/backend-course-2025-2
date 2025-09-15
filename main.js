df7423058fde8388548d8f111bf271eea7f33626
Oleksii bc2025-2 <rabinecoleksij@gmail.com>
2025-09-15 23:09:28 +0300

    Add Node.js script for part III

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+const currency = "UAH";
+const exact_date = "2024-09-10";
+const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/secondarymarket");
+url.searchParams.append("currency", currency);
+url.searchParams.append("date", exact_date);
+console.log(url.toString());
